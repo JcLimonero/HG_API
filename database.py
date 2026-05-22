4578988c1e360b9d727b23add85362f12f6a98fe
MrCodeBender <mr.code.bender@gmail.com>
2026-05-21 18:07:51 -0600

    refactor: format paginated query string for improved readability

--- database.py
+++ database.py
@@ -42,8 +42,7 @@ Where o.Situacao ='R'
 """
 
 PAGINATED_QUERY = """
-select TOP {page_size} SKIP {offset}
-  o."Nr OS" as 'Orden de Reparacion',
+select TOP {page_size} SKIP {offset} o."Nr OS" as 'Orden de Reparacion',
   a."Chassi" as 'Numero de Chasis',
   v."Nm Modelo" as 'Modelo',
   v."Versao" as 'Version',
